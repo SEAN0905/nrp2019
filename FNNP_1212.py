3de7bbcdb1906cb0df4b4e8eec39642064ed44e2
SEAN0905 <seanxinyu2@gmail.com>
2019-12-14 00:24:37 +0800

    implementation of basic structure but bug present

--- FNNP_1212.py
+++ FNNP_1212.py
@@ -28,30 +28,34 @@ class GAP():
         optimizer = SGD(lr=0.01, momentum=0.9)
 
         # build discriminator
-        self.discriminator =  self.build_discriminator()
+        self.discriminator = self.build_discriminator()
         self.discriminator.compile(loss="categorical_crossentropy",
                 optimizer=optimizer,
                 metrics=['accuracy'])
-        # for the combined model only discriminator will be trained
-        self.discriminator.trainable = False
 
         # build generator
         self.generator = self.build_generator()
 
         # to sepcify the input and output of the GAP
-        z = Input(shape=(self.img_shape))
+        z = Input(shape=(1124, ))
         img_prv = self.generator(z)
+
+        # for the combined model only discriminator will be trained
+        self.discriminator.trainable = False
+
         clasif_res = self.discriminator(img_prv)
 
         # TODO:to be tuned
         # the weight of the adversary loss
-        loss_x = 0.1
+        self.loss_x = 0.1
 
+        # the model yielf two results: img_prv and clasif_res
+        # but with only one output
         self.combined = Model(z, clasif_res)
-        self.combined.compile(optimizer=optimizer, loss=["categorical_crossentropy", self.X_loss], loss_weights=[1, loss_x])
+        self.combined.compile(optimizer=optimizer, loss=self.X_loss, loss_weights=[self.loss_x])
 
     def X_loss(self, y_true, y_pred):
-        return K.mean(K.square(y_true - y_pred) / (255.0 * 255.0))
+        return K.mean(K.square(y_true - y_pred))
 
     def read_data(self):
         # gender.txt: 0 for woman, 1 for man
@@ -156,14 +160,21 @@ class GAP():
 
         model.summary()
 
+        model.load_weights("adversary32.h5")
+
         img_prv = Input(shape=self.img_shape)
         clasify_res = model(img_prv)
+
+
 
         return Model(img_prv, clasify_res)
     
     def train(self, epochs, batch_size=64, sample_interval=50):
         # load raw data
         X_data_raw, Y_gender_raw, Y_smile_raw = self.read_data()
+
+        # print(X_data_raw.shape, Y_gender_raw.shape, Y_smile_raw.shape)
+        # # (2723, 1024) (2723, 2) (2723, 2)
 
         for epoch in range(epochs):
 
@@ -172,7 +183,7 @@ class GAP():
             # --------------------
 
             # select random batch of images
-            ids = np.random.randint(0, X_data_raw[0], batch_size)
+            ids = np.random.randint(0, 2723-1, batch_size)
             imgs = X_data_raw[ids]
             gender_label =  Y_gender_raw[ids]
             smile_label = Y_smile_raw[ids]
@@ -193,8 +204,18 @@ class GAP():
 
             # train the discriminator
             d_loss_prv = self.discriminator.train_on_batch(prv_imgs, gender_label)
-            d_loss_act = self.discriminator.train_on_batch(imgs, gender_label)
-            d_loss = p * np.add(d_loss_prv, d_loss_act)
+            # print(d_loss_prv)
+            
+            # ---------------------
+            #  Train Generator
+            # ---------------------
+            g_loss = self.combined.train_on_batch(img_cons, gender_label)
+            print("Epoch {0:3} [D loss: {1:20}, acc.: {2:8}%] [G loss: {3:20}]".format(epoch, d_loss_prv[0], 100*d_loss_prv[1], g_loss))
+
+
+if __name__ == "__main__":
+    gap = GAP()
+    gap.train(epochs=20)
 
             
 
